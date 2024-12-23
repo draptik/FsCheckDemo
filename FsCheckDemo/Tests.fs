052334a510a1a8b1627e3596da4fa647a19860f8
Patrick Drechsler <patrick@pdrechsler.de>
2024-12-23 02:03:53 +0100

    🎉 initial commit

---
+++ FsCheckDemo/Tests.fs
@@ -0,0 +1,15 @@
+﻿module Tests
+
+open Xunit
+open FsCheck
+open Swensen.Unquote
+
+[<Fact>]
+let ``reversing a list twice returns original list`` () =
+    
+    let checkFn (aList: int list) =
+        let actual = aList |> List.rev |> List.rev
+        let expected = aList
+        test <@ actual = expected @>
+        
+    Check.Verbose checkFn
